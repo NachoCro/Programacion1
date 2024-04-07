a4f6067831223ddf7b684dbc87fdf17f710af485
NachoCro <Nachossjb@gmail.com>
2024-04-07 15:40:04 -0300

    Programacion1, 5/4

---
+++ Practica/Operadores aritmeticos/Trabajo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float numero1;
+    float numero2;
+    float resultado;
+
+    numero2 = 2;
+    numero1 = 1;
+
+    resultado = numero1 + numero2; // SUMA
+    //resultado = numero1 - numero2; // Resta
+    //resultado = numero1 * numero2; // Multiplicacion
+    //resultado = numero1 / numero2; // Divicion
+    //resultado = numero1 % numero2; // Resto
+
+
+    cout << "El resultado es: " << resultado << endl;
+
+    return 0;
+}
