025fba80a7fa823e9ee6180af710fba81f2c020c
NachoCro <110935501+NachoCro@users.noreply.github.com>
2025-08-04 11:09:30 -0300

    Actualizar Actividad.cpp

--- Practica/actividad1/Actividad.cpp
+++ Practica/actividad1/Actividad.cpp
@@ -2,22 +2,22 @@
 
 using namespace std;
 
-// Ingresar las variables float (masa y aseleracion)
+// Ingresar las variables float (masa y aceleracion)
 // pedimos los datos al usuario
-// Calcular la fuerza (Fuerza = Masa * aseleracion)
+// Calcular la fuerza (Fuerza = Masa * aceleracion)
 // Mostrar el resultado del calculo osea la fuerza
 
 
 int main (){
 
     float MasaDelObjeto;
-    float AseleracionDelObjeto;
+    float AceleracionDelObjeto;
     cout << "Ingrese la masa ";
     cin >> MasaDelObjeto;
     cout << "ingrese la aseleracion ";
-    cin >> AseleracionDelObjeto;
+    cin >> AceleracionDelObjeto;
 
-    float FuerzaDelObjeto = MasaDelObjeto * AseleracionDelObjeto;
+    float FuerzaDelObjeto = MasaDelObjeto * AceleracionDelObjeto;
 
     cout << "La fuerza es de: " << FuerzaDelObjeto;
     return 0;
